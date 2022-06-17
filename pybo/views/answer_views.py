0ec78b84e5db42f4e01bb6eb941aa82aa156e1c3
yeseong31 <yeseong31@naver.com>
2022-06-17 20:26:53 +0900

    0617 pybo 3-12 anchor

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotAllowed
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from ..forms import AnswerForm
@@ -22,7 +22,8 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question_id)
+            # return redirect('pybo:detail', question_id=question_id)
+            return redirect(f'{resolve_url("pybo:detail", question_id=question_id)}#answer_{answer.id}')
     else:
         return HttpResponseNotAllowed('POST 요청만 가능합니다.')
     context = {'question': question, 'form': form}
@@ -37,14 +38,16 @@ def answer_modify(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
     if request.user != answer.author:
         messages.error(request, '답변 수정 권한이 없습니다.')    # messages: Django 모듈(넌필드 오류 발생 시 사용)
-        return redirect('pybo:detail', question_id=answer.question.id)
+        # return redirect('pybo:detail', question_id=answer.question.id)
+        return redirect(f'{resolve_url("pybo:detail", question_id=answer.question.id)}#answer_{answer.id}')
     if request.method == 'POST':
         form = AnswerForm(request.POST, instance=answer)
         if form.is_valid():
             answer = form.save(commit=False)
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('pybo:detail', question_id=answer.question.id)
+            # return redirect('pybo:detail', question_id=answer.question.id)
+            return redirect(f'{resolve_url("pybo:detail", question_id=answer.question.id)}#answer_{answer.id}')
     else:
         form = AnswerForm(instance=answer)
     context = {'answer': answer, 'form': form}
@@ -61,4 +64,5 @@ def answer_delete(request, answer_id):
         messages.error(request, '답변 삭제 권한이 없습니다.')  # messages: Django 모듈(넌필드 오류 발생 시 사용)
     else:
         answer.delete()
-    return redirect('pybo:detail', question_id=answer.question.id)
+    # return redirect('pybo:detail', question_id=answer.question.id)
+    return redirect(f'{resolve_url("pybo:detail", question_id=answer.question.id)}#answer_{answer.id}')
