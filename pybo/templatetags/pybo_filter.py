9db3d66ef3c759fe72a970b131f0871bed4f7bcb
yeseong31 <yeseong31@naver.com>
2022-06-17 20:38:42 +0900

    0617 pybo 3-13 markdown

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,6 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -7,3 +9,10 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+# 마크다운 적용
+@register.filter()
+def md(value):
+    extensions = ['nl2br', 'fenced_code']
+    return mark_safe(markdown.markdown(value, extensions=extensions))
