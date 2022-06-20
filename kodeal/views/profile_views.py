942ecab3e33ab9a3e7be9516ef5e9c456e7f08fe
yeseong31 <yeseong31@naver.com>
2022-06-20 16:01:14 +0900

    0620 @login_required 추가

--- kodeal/views/profile_views.py
+++ kodeal/views/profile_views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required(login_url='common:signin')
 def index(request):
     """
     마이페이지(프로필)
