ee40b744a0c2518d7340f490726891cee3e168d0
yeseong31 <yeseong31@naver.com>
2022-06-20 17:30:14 +0900

    0620 DB Model 작성

--- kodeal/models.py
+++ kodeal/models.py
@@ -1,3 +1,92 @@
+import uuid as uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Keyword(models.Model):
+    """
+    OpenAI Codex Keyword Model
+    """
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=100, verbose_name='키워드')
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'kodeal_keyword'
+
+
+class Question(models.Model):
+    """
+    OpenAI Codex Question Model
+    """
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_q',
+                               verbose_name='질문 작성자')
+    content = models.TextField(verbose_name='질문 내용')
+    language = models.CharField(max_length=30, null=True, blank=True, verbose_name='사용 언어')
+    voter = models.ManyToManyField(User, related_name='voter_q', verbose_name='추천인')
+    keyword = models.ManyToManyField(Keyword, related_name='keyword_q', verbose_name='키워드')
+    create_date = models.DateTimeField(verbose_name='생성일')
+    modify_date = models.DateTimeField(null=True, blank=True, verbose_name='수정일')
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'kodeal_codex_question'
+
+
+class Answer(models.Model):
+    """
+    OpenAI Codex Answer Model
+    """
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_a',
+                               verbose_name='답변 작성자')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='연관된 질문')
+    content = models.TextField(verbose_name='답변 결과')
+    voter = models.ManyToManyField(User, related_name='voter_a', verbose_name='추천인')
+    create_date = models.DateTimeField(verbose_name='생성일')
+    modify_date = models.DateTimeField(null=True, blank=True, verbose_name='수정일')
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'kodeal_codex_answer'
+
+
+class Papago(models.Model):
+    """
+    Papago Answer Model
+    """
+    id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='연관된 질문')
+    content = models.TextField(verbose_name='번역 결과')
+    create_date = models.DateTimeField(verbose_name='생성일')
+    modify_date = models.DateTimeField(null=True, blank=True, verbose_name='수정일')
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'kodeal_papago'
+
+
+class Profile(models.Model):
+    """
+    Kodeal Profile(Image) Model
+    """
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True, verbose_name='이미지')
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, verbose_name='UUID')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='image_owner', verbose_name='이미지 소유자')
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'kodeal_profile'
