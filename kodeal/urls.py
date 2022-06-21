fb862d658bb1a5c4e63fa2ec6c10a2d6536d95f5
yeseong31 <yeseong31@naver.com>
2022-06-21 09:15:31 +0900

    0620 move qna index()

--- kodeal/urls.py
+++ kodeal/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     # ----- base_views.py -----
     # 메인 페이지
     path('', base_views.index, name='index'),
-    # QnA 메인 페이지
-    path('qna/', base_views.qna, name='qna'),
 
     # ----- profile_views.py -----
     # 마이 페이지
     path('profile/', profile_views.index, name='profile'),
 
     # ----- question_views.py -----
+    # QnA 페이지
+    path('qna/', question_views.qna, name='qna'),
     # 질문 등록
     path('question/create/', question_views.question_create, name='question_create'),
 ]
--- kodeal/views/question_views.py
+++ kodeal/views/question_views.py
@@ -1,11 +1,36 @@
+import math
+
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
 from django.utils import timezone
 
 from kodeal.forms import QuestionForm
-from kodeal.models import Answer, Keyword
+from kodeal.models import Answer, Keyword, Question
 from kodeal.views.api.codex import question_to_codex, extract_answer_sentences, get_answer_and_keyword
 from kodeal.views.api.papago import papago
+
+
+@login_required(login_url='common:signin')
+def qna(request):
+    """
+    Kodeal QnA Page
+    """
+    question_list = Question.objects.order_by('-create_date')
+
+    # 페이징 처리
+    page = request.GET.get('page', '1')
+    paginator = Paginator(question_list, 10)  # 한 페이지 당 10개의 질문
+    page_obj = paginator.get_page(page)  # 페이징 객체 생성
+    # 시작 번호
+    temp_end = int(math.ceil(page_obj.number / 10.0)) * 10
+    start = temp_end - 9
+    # 끝 번호
+    total_page = math.ceil(len(question_list) / 10)
+    end = temp_end if total_page > temp_end else total_page
+
+    context = {'question_list': page_obj, 'start': start, 'end': end, 'total_page': total_page, 'page': page}
+    return render(request, 'kodeal/qna/index.html', context)
 
 
 @login_required(login_url='common:signin')
