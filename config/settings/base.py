f7d92ba4f24a2ae8a18d993ae50feeb4577ea7df
예성 <yeseong31@naver.com>
2022-06-26 23:10:04 +0900

    0626 static 경로 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -28,6 +28,7 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,5 @@
 from .base import *
 
 ALLOWED_HOSTS = ['43.200.23.92']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
