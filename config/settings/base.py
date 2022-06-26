8e01029d486dcabefddc9810b8a036f3b39b354a
예성 <yeseong31@naver.com>
2022-06-26 18:01:42 +0900

    0626 settings.py 분리

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-from config import my_settings
+from config.settings import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
