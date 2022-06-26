cdbb453f27e1588bf0f66f4f72b7b7539c61a81c
예성 <yeseong31@naver.com>
2022-06-26 17:52:04 +0900

    0626 EC2 서버 접속 테스트

--- config/settings/base.py
+++ config/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.200.23.92']
 
 
 # Application definition
